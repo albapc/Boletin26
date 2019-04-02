8b662c97915b7b33999238c6a7b4b6306ad70b42
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-04-02 11:40:48 +0200

    commit final

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,11 @@
+
+package boletin26;
+
+public class Boletin26 {
+
+    public static void main(String[] args) {
+        XestionAlumnos x = new XestionAlumnos();
+        x.setVisible(true);
+    }
+    
+}
